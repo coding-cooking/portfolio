e1139b668a070172a26a3ff05570e26f1cabba6f
Coding-Cooking <fei.coding2023@gmail.com>
2023-11-13 15:04:50 +1100

    update some new data

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -51,7 +51,7 @@ const About: FC<{}> = () => {
 					})} */}
 					{
 						lang === 'EN' ? <>
-							<p>When I was a senior CMS manager 6 years ago, I made this career
+							<p>When I was a senior CMS manager 4 years ago, I made this career
 								transition to become a software developer. I love getting hands-on
 								with Tech, and I have found that working as a software developer, I
 								get more exposure and hands-on with technology than any other role I
@@ -61,7 +61,7 @@ const About: FC<{}> = () => {
 
 						</> : <>
 							<p style={{ lineHeight: 1.6 }}>
-								6年前，当我还是一名高级内容管理系统（CMS）经理时，我做出了职业转型的决定，成为一名软件开发人员。我喜欢亲自动手进行技术工作，而我发现作为一名软件开发人员，我比我之前从事的任何其他职位都更多地接触和实践技术。尽管如此，我个人并不认为之前的时间是浪费的，因为它为我提供了宝贵的CMS行业洞察。</p>
+								4年前，当我还是一名高级内容管理系统（CMS）经理时，我做出了职业转型的决定，成为一名软件开发人员。我喜欢亲自动手进行技术工作，而我发现作为一名软件开发人员，我比我之前从事的任何其他职位都更多地接触和实践技术。尽管如此，我个人并不认为之前的时间是浪费的，因为它为我提供了宝贵的CMS行业洞察。</p>
 						</>
 					}
 
--- src/constants/job.ts
+++ src/constants/job.ts
@@ -12,16 +12,16 @@ export type JobYear = JobInterface['year']
 export const yearsEn: Array<JobYear> = [
     '07/2023 to date',
     '10/2022 - 06/2023',
-    '06/2018 - 09/2022',
-    '12/2014 - 05/2018',
+    '06/2020 - 09/2022',
+    '12/2014 - 05/2020',
     '07/2011 - 11/2014'
 ]
 
 export const yearsCH: Array<JobYear> = [
     '07/2023 to date',
     '10/2022 - 06/2023',
-    '06/2018 - 09/2022',
-    '12/2014 - 05/2018',
+    '06/2020 - 09/2022',
+    '12/2014 - 05/2020',
     '07/2011 - 11/2014'
 ]
 
@@ -42,28 +42,28 @@ const jobsEn: Array<JobInterface> = [
         year: '10/2022 - 06/2023',
         company: 'Duozhuayu',
         jobTitle: 'Frontend Developer',
-        jobDescription: 'The toC user interface using react, typescript, styled components to build responsive UI with Storybook, using axios to fetch data and redux to manage the state; The in-house CMS system using react & typescript & ant-design & redux to manage the warehouse stock; Working with a dev team of 10 people, mutually review code and address pr feedbacks, split tasks and assign to team members regularly',
+        jobDescription: 'Building responsive to C user interface using react, NextJs, typescript, styled components, and storybook, using Axios to fetch data and redux to manage the state.; Building the in-house CMS system, Using react, typescript, ant-design and redux; Working with a dev team of 10 people, mutually review code and address pr feedbacks, split tasks and assign to team members regularly. ',
         produceImage: '',
     },
     {
-        year: '06/2018 - 09/2022',
+        year: '06/2020 - 09/2022',
         company: 'UC Browser',
         jobTitle: 'Frontend Developer',
-        jobDescription: 'Writing modern, performant, maintainable code for multiple content management systems; Using different languages, frameworks, and libraries such as JavaScript, TypeScript, React, Redux, Ant Design, Styled Components, Node.js; Communicating with multi - disciplinary teams of engineers, product managers on a regular basis; Providing effective troubleshooting and remediation for web applications',
+        jobDescription: 'Writing modern, performant, maintainable code for multiple content management systems.; Using different languages, frameworks, and libraries such as JavaScript, TypeScript, React, Redux, Ant Design, Styled Components, Node.js, WebSocket. ; Communicating with multi-disciplinary teams of engineers, product managers on a regular basis.;Providing effective troubleshooting and remediation for web applications. ',
         produceImage: '',
     },
     {
-        year: '12/2014 - 05/2018',
+        year: '12/2014 - 05/2020',
         company: 'Toutiao',
         jobTitle: 'Content Operation Manager',
-        jobDescription: 'Select breaking news and high-quality content, utilize advanced personalized recommendation technology to deliver to users, making our APP the most popular news app in China; Collaborate with product and development teams to create new consumer products and build internal operational tools; Responsible for discussing with UI & UX about then content display and user interaction of website content, using sketches and Lark, etc',
+        jobDescription: 'Utilizing advanced personalized algorithms, in combination with human operation to recommend customized content and breaking news to users.;Collaborating with product managers and development teams to build, optimize, and updata multiple content management systems, enabling the operation team to promptly discover breaking news and trending topics.; Responsible for discussing with UI & UX about then content display and user interaction of website content, using sketches and Lark, etc. ',
         produceImage: '',
     },
     {
         year: '07/2011 - 11/2014',
         company: 'NetEase',
         jobTitle: 'Content Operations',
-        jobDescription: 'Writing and publishing news and anecdotes around the world on website, also dealing with comments and interact with viewers',
+        jobDescription: 'Selecting major global news and provide recommendations on the website.;Planning special coverage for major news events, creating original content, H5 pages, image galleries, and more.',
         produceImage: '',
     }
 ];
@@ -80,28 +80,28 @@ const jobsCH: Array<JobInterface> = [
         year: '10/2022 - 06/2023',
         company: '多抓鱼',
         jobTitle: '前端开发',
-        jobDescription: '使用React、Typescript和Styled Components构建响应式用户界面，使用Storybook进行UI开发，使用axios获取数据，使用Redux管理状态来构建ToC用户界面; 同时，使用React、Typescript、Ant Design和Redux来管理仓库库存的内部CMS系统; 与一个由10人组成的开发团队合作，定期相互审查代码并处理PR反馈，拆分任务并分配给团队成员',
+        jobDescription: '使用React、NextJs、Typescript和Styled Components构建响应式用户界面，使用Storybook进行UI开发，使用axios获取数据，使用Redux管理状态来构建ToC用户界面; 同时，使用React、Typescript、Ant Design和Redux来管理仓库库存的内部CMS系统; 与一个由10人组成的开发团队合作，定期相互审查代码并处理PR反馈，拆分任务并分配给团队成员',
         produceImage: '',
     },
     {
-        year: '06/2018 - 09/2022',
+        year: '06/2020 - 09/2022',
         company: 'UC浏览器',
         jobTitle: '前端开发',
-        jobDescription: '编写现代、高性能、易维护的代码，适用于多个内容管理系统; 使用不同的语言、框架和库，如JavaScript、TypeScript、React、Redux、Ant Design、Styled Components、Node.js; 与跨部门的工程师和产品经理团队进行定期沟通; 为Web应用程序提供有效的故障排除和修复',
+        jobDescription: '为多个内容管理系统编写现代化、高性能、可维护的代码。;使用不同的技术语言、框架和库，如JavaScript、TypeScript、React、Redux、Ant Design、Styled Components、Node.js、WebSocket。;与工程师、产品经理等多学科团队定期沟通；为Web应用程序提供有效的故障排除和修复服务。',
         produceImage: '',
     },
     {
-        year: '12/2014 - 05/2018',
+        year: '12/2014 - 05/2020',
         company: '头条',
         jobTitle: '内容运营经理',
-        jobDescription: '选择突发新闻和高质量内容，利用先进的个性化推荐技术向用户提供，使我们的APP成为中国最受欢迎的新闻应用; 与产品和开发团队合作，创建新的消费产品并构建内部运营工具; 负责与UI和UX讨论网站内容的展示和用户互动，使用草图和Lark等工具',
+        jobDescription: '利用先进的个性化算法，结合人工运营，向用户推送定制内容和突发新闻。;与产品经理和开发团队合作，打造、优化和升级多个内容管理系统，使运营团队能够及时发现突发新闻和热门话题。;负责与UI和UX讨论和改进产品的UI与功能设计，提升用户体验。',
         produceImage: '',
     },
     {
         year: '07/2011 - 11/2014',
         company: '网易',
         jobTitle: '内容运营',
-        jobDescription: '在网站上撰写和发布世界各地的新闻和趣闻，同时处理评论并与观众互动',
+        jobDescription: '筛选全球重大实时新闻，在网站上提供推荐和展示。;为重大新闻事件策划特别报道，创作原创内容、H5页面、图集等。',
         produceImage: '',
     }
 ]
